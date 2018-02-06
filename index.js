cbe50c0b0ef358d2377493931d665b9892291251
Mila J <icecold240@gmail.com>
2018-02-06 03:52:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ function dec(n) {
 //   return n;
 // }
 
-function preserveDecimal(n) {
-  parseFloat(n);
-  return n;
-}
+// function preserveDecimal(n) {
+//   parseFloat(n);
+//   return n;
+// }

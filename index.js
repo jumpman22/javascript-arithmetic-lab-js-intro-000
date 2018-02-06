feb21a98ccdd60ac6e93e58d292bae5c53018b81
Mila J <icecold240@gmail.com>
2018-02-06 04:17:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,11 +25,9 @@ function dec(n) {
 }
 
 function makeInt(n) {
-  parseInt(n);
-  return n;
+  return parseInt(n);
 }
 
 function preserveDecimal(n) {
-  parseFloat(n);
-  return n;
+  return parseFloat(n);
 }
